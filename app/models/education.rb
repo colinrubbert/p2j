c4b26c58c53be16d4a04064590c6cbc3314bb600
Colin Rubbert <colinrubbert@gmail.com>
2016-04-16 09:49:35 -0400

    build logic into education model

--- app/models/education.rb
+++ app/models/education.rb
@@ -1,2 +1,35 @@
 class Education < ActiveRecord::Base
+
+  COURSE_TYPES = {
+    'Online Self Paced' => 'Online Self Paced',
+    'Online Bootcamp' => 'Online Bootcamp',
+    'Campus Bootcamp' => 'Campus Bootcamp'
+  }
+
+  COURSE_CATEGORY = {
+    'Full-Stack Developer' => 'Full-Stack Developer',
+    'Front-End Developer' => 'Front-End Developer',
+    'UI/UX Developer' => 'UI/UX Developer',
+    'Backend Developer' => 'Backend Developer'
+  }
+
+  COURSE_LANGUAGE = {
+    'Ruby on Rails' => 'Ruby on Rails',
+    'JavaScript' => 'JavaScript',
+    'ReactJS' => 'ReactJS',
+    'Swift' => 'Swift',
+    'AngularJS' => 'AngularJS',
+    'MeteorJS' => 'MeteorJS',
+    'C#' => 'C#',
+    'Python' => 'Python'
+  }
+
+  COURSE_LOCATION = {
+    'Online' => 'Online',
+    'Miami' => 'Miami',
+    'Atlanta' => 'Atlanta',
+    'Ft. Lauderdale' => 'Ft. Lauderdale',
+    'London' => 'London'
+  }
+
 end
